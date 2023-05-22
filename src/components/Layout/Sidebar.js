2181290ecf80210775af0504a19deec5d7125e43
Ravindu Ranaweera <ravindu.ranaweera@pearson.com>
2023-05-22 20:21:35 +0530

    chnge link

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -34,6 +34,10 @@ const Sidebar = () => {
 
             ),
             subMenus: [
+                {
+                    name: "User Story",
+                    link: "/dashboard",
+                  },
             ],
         },
         {
